d7ff138053d880b70a678107367a4e0af52f1000
JeanPaul Attard <jeanp.attard@gmail.com>
2017-10-22 18:22:15 +0200

    - The merge -m, --merge command line option is now accepted

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ var processor = require('./scripts/processor');
 
 program
     .option('-r, --reverse', 'Perform the reverse process, creating a json structure from a .properties file')
+    .option('-m, --merge [file name]', 'Bundles the content of all the generated properties file into one file, prefixed by the name of the file. When used with -r --reverse option, the specified file is unmerged in separate json files represented by the initial key of each group.')
     .option('-s, --spaces <spaces>', 'The amount of spaces per line during the properties to json conversion. Defaults to 4.')
     .option('-c, --config <config>', 'A file in json format having a src and dist attribute, pointing to the source directory where the input files are located, and a destination directory where the output files are written.')
     .parse(process.argv);
@@ -18,6 +19,16 @@ if (program.reverse) {
 
 if (program.spaces) {
     options.spaces = +program.spaces;
+}
+
+if (program.merge) {
+    // If no file name was provided, program.merge has value 'true'
+    if (typeof program.merge === 'boolean') {
+        console.log('No file name was provided when using the -m, --merge option. Defaulting to bundle.properties...');
+        options.merge = 'bundle.properties';
+    } else {
+        options.merge = program.merge;
+    }
 }
 
 if (program.config) {
